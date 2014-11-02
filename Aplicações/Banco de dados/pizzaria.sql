dd4a713131e24c58110f0e98e0741363137230a2
raphael.v.work <raphael.v.work@gmail.com>
2014-11-02 16:06:02 +0000

    Atualizações do BD, provavelmente esse é o BD final.

--- Aplicações/Banco de dados/pizzaria.sql
+++ Aplicações/Banco de dados/pizzaria.sql
@@ -50,8 +50,8 @@ Numero_Residencia VARCHAR(6),
 CEP_Funcionario VARCHAR(10),
 Estado_Funcionario VARCHAR(2),
 Cidade_Funcionario VARCHAR(20),
-Email_Funcionario VARCHAR(40),
 Bairro_Funcionario VarChar (30),
+Email_Funcionario VARCHAR(40),
 Celular_Funcionario VarChar (15),
 Telefone_Funcionario VarChar (14),
 Cod_Permissao INT FOREIGN KEY REFERENCES Permissao(Cod_Permissao),
@@ -125,13 +125,7 @@ Senha VARCHAR(20),
 Cod_Funcionario INT FOREIGN KEY REFERENCES Funcionario(Cod_Funcionario),
 Cod_Permissao INT FOREIGN KEY REFERENCES Permissao(Cod_Permissao)
 )
-go
 
-create table Categoria
-(
-Cod_Categoria INT IDENTITY(1,1) PRIMARY KEY,
-Nome_Categoria VARCHAR(30)
-)
 go
 
 create table Produto
@@ -141,7 +135,7 @@ Nome_Produto VARCHAR(40),
 Valor_Venda DECIMAL(6,2),
 Ingred_Prod VARCHAR(100),
 Sobe_Site INT,
-Cod_Categoria INT FOREIGN KEY REFERENCES Categoria(Cod_Categoria)
+
 )
 go
 
@@ -177,19 +171,25 @@ Estado_Fornecedor VarChar (2),
 Cidade_Fornecedor VarChar (20),
 Bairro_Fornecedor VarChar (30),
 Complemento VarChar (40),
-Cod_Produto INt Foreign Key References Pedido (Cod_Pedido),
 )
 go
 
+
 create table Insumo_Fornecedor
 (
+Cod_InsumoFornecedor int identity (1,1) Primary Key,
 Cod_Insumo INT FOREIGN KEY REFERENCES Insumo(Cod_Insumo),
 Cod_Fornecedor INT FOREIGN KEY REFERENCES Fornecedor(Cod_Fornecedor),
 )
 go
 
+
+
+
 create table Produto_Insumo
+
 (
+Cod_ProdutoInsumo int identity (1,1) Primary Key,
 Cod_Insumo INT FOREIGN KEY REFERENCES Insumo(Cod_Insumo),
 Cod_Produto INT FOREIGN KEY REFERENCES Produto(Cod_Produto),
 )
@@ -325,32 +325,36 @@ insert into FuncPermissao(Login_, Senha)
 values ('admin','1234'),('Tuca','123456')
 go
 
-insert into Categoria(Nome_Categoria)
-values
-('Pizza Grande'),('Pizza Brotinho'),('Bebidas'),('Porções')
-go
 
 insert into Produto
 (
 Nome_Produto,
 Valor_Venda,
 Ingred_Prod,
-Sobe_Site,
-Cod_Categoria
+Sobe_Site
 )
 values
-('Pizza Baiana',20.00,'Queijo Mussarela, Calabresa Moída, Ovos, Cebola e Pimenta.',1,1),('Pizza Mussarela',18.40,'Queijo Mussarela e Cebola.',1,1),('Pizza Bacon',20.20,'Queijo Mussarela, Ovos e Bacon.',1,1),
-('Pizza Americana',24.00,'Queijo Mussarela, Lombo, Champignon e Palmito.',1,1),('Pizza Bauru',23.50,'Queijo Mussarela, Presunto e Tomate.',1,1),('Pizza Calabresa',18.00,'Queijo Mussarela, Calabresa e Cebola.',1,1),
-('Pizza Catupiry',23.00,'Queijo Mussarela e Catupiry.',1,1),('Pizza Três Queijos',24.70,'Queijo Mussarela, Catupiry e Provolone',1,1),('Pizza Alemã',25.20,'Queijo Mussarela, Calabresa Moída e Parmesão.',1,1),
-('Pizza Havaiana Brotinho',13.70,'Queijo Mussarela, ',0,2),('Pizza Baiana Brotinho',14.50,'Queijo Mussarela, ',0,2),('Pizza Palmito Brotinho',15.00,'Queijo Mussarela, ',0,2),('Pizza Peruana Brotinho',14.00,'Queijo Mussarela, ',0,2),
-('Refrigerante',7.00,null,0,3),('Cerveja',9.40,null,0,3),('Vinho',15.50,null,0,3),('Champagne',17.00,null,0,3),
-('Porção de Camarão',19.00,'Porção de Camarão, acompanhada de cebola',0,4),('Porção de Calabresa',18.00,'Porção de Calabresa, acompanhada de cebola',0,4),('Porção de Provolone',17.00,'Porção de Provolone, acompanhada de cebola',0,4)
+('Pizza Baiana',20.00,'Queijo Mussarela, Calabresa Moída, Ovos, Cebola e Pimenta.',1),('Pizza Mussarela',18.40,'Queijo Mussarela e Cebola.',1),('Pizza Bacon',20.20,'Queijo Mussarela, Ovos e Bacon.,',1),
+('Pizza Americana',24.00,'Queijo Mussarela, Lombo, Champignon e Palmito.',1),('Pizza Bauru',23.50,'Queijo Mussarela, Presunto e Tomate.',1),('Pizza Calabresa',18.00,'Queijo Mussarela, Calabresa e Cebola.',1),
+('Pizza Catupiry',23.00,'Queijo Mussarela e Catupiry.',1),('Pizza Três Queijos',24.70,'Queijo Mussarela, Catupiry e Provolone',1),('Pizza Alemã',25.20,'Queijo Mussarela, Calabresa Moída e Parmesão.',1),
+('Pizza Havaiana Brotinho',13.70,'Queijo Mussarela, ',1),('Pizza Baiana Brotinho',14.50,'Queijo Mussarela, ',0),('Pizza Palmito Brotinho',15.00,'Queijo Mussarela, ',0),('Pizza Peruana Brotinho',14.00,'Queijo Mussarela, ',1),
+('Refrigerante',7.00,null,0),('Cerveja',9.40,null,0),('Vinho',15.50,null,0),('Champagne',17.00,null,0),
+('Porção de Camarão',19.00,'Porção de Camarão, acompanhada de cebola',0),('Porção de Calabresa',18.00,'Porção de Calabresa, acompanhada de cebola',0),('Porção de Provolone',17.00,'Porção de Provolone, acompanhada de cebola',1)
 go
 
 insert into Pedido(Cod_Cliente,Cod_Funcionario,Data,Hora,Valor)
 values
-(1,1,'10/05/2014','20:15',31),(2,2,'22/07/2014','19:14',25.25),(3,3,'12/08/2014','22:57',38.89),(3,1,'04/10/2014','21:40',78.98)
+(1,1,'10/05/2014','20:15',31.00),
+(2,2,'22/07/2014','19:14',25.25),
+(3,3,'12/08/2014','22:57',38.89),
+(3,1,'04/10/2014','21:40',78.98),
+(5,2,'02/07/2014','21:15',42.30),
+(5,3,'05/02/2014','20:22',67.90),
+(5,2,'15/06/2014','22:57',84.20),
+(4,1,'29/04/2014','18:49',76.00),
+(2,3,'30/02/2014','19:16',58.20)
 go
+
 
 insert into Fornecedor
 (
@@ -370,14 +374,16 @@ CEP_Fornecedor,
 Estado_Fornecedor,
 Cidade_Fornecedor,
 Bairro_Fornecedor,
-Complemento,
-Cod_Produto /*ESSE VALOR É INTEIRO*/
+Complemento
 )
 values
-(22222222222, 'Caixa de Queijo Ltda'),
-(33333333333, 'Embalagens São Paulo EPP'),
-(11111111111, 'Na Lata - Enlatados em geral Ltda'),
-(44444444444, 'Viva Verde Hortaliças EPP')
+('222-222-222-22', 'Caixa de Queijo Ltda', 'Queijo in Box', 'Itaú', '0000', '1111-0', 'John Cheese', '91111-1111', 'jc_queijos@jc.com', '(11)2222-3333', 'Rua dos Tupinambás', '456', '01234-000', 'SP', 'São Paulo', 'Jardim Mundial', '6º Andar'),
+
+('333-333-333-33', 'Embalagens São Paulo EPP', 'SP Embalagens', 'Banco do Brasil', '1234', '4321-5', 'Josué Alvarenga', '95555-6666', 'embalagens@sp.com.br', '(11)1111-9999', 'Avenida Cascatinhas', '881', '09876-123', 'SP', 'Suzano', 'Vila Emerson', 'Sobreloja'),
+
+('111-111-111-11', 'Na Lata - Enlatados em geral Ltda', 'Na Lata!', 'Bradesco', '0055', '055678-2', 'Clóvis Ribeiro', '98888-4444', 'enlatados@clovis.com', '(13)3456-7890', 'Avenida da Orla', '200', '02345-987', 'SP', 'Santos', 'Paquetá', 'Zona Portuária'),
+
+('444-444-444-44', 'Hortaliças Hortolândia EPP', 'Viva Verde Hortaliças', 'Santander', '9966', '013568-7', 'Romualdo Nunes', '97777-5501', 'rn@vivaverde.com.br', '(11)2233-5678', 'Rua do Chá', '56', '03456-321', 'SP', 'São Paulo', 'Ceasa', 'Box 100')
 go
 
 insert into Promocao
