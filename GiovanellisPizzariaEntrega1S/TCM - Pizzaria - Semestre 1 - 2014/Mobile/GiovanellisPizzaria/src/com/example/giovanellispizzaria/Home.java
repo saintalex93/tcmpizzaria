c2aed2af1b1590fb8db144ac363830a1af25d592
arthurborbalopes <arthurborbalopes@gmail.com>
2015-03-24 03:24:12 +0000

    Leve mexida no projeto

--- GiovanellisPizzariaEntrega1S/TCM - Pizzaria - Semestre 1 - 2014/Mobile/GiovanellisPizzaria/src/com/example/giovanellispizzaria/Home.java
+++ GiovanellisPizzariaEntrega1S/TCM - Pizzaria - Semestre 1 - 2014/Mobile/GiovanellisPizzaria/src/com/example/giovanellispizzaria/Home.java
@@ -1,27 +1,45 @@
 package com.example.giovanellispizzaria;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Home extends Activity {
 
 	TextView saudacao;
+	Button botao;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 		
+		Button btn = (Button) findViewById(R.id.button1);
+
+		TextView text = (Button) findViewById(R.id.txtSaudacao);
+		
+		text.setText(text.getText().toString() + " Roberto!");
+		
+		btn.setOnClickListener(new View.OnClickListener() {
+
+		    public void onClick(View v) {
+		        myClick(v); /* my method to call new intent or activity */
+		    }
+		    
+		    public void myClick(View v) {
+		    	 
+		    }
+		});
+		
 		//final String tag = "Home";
 		
-	//	 saudacao = (TextView)findViewById(R.string.saudacao_home);
-	
-//			Log.d(tag, saudacao.getText(saudacao.setText(saudacao.getText() + "Roberto")));
+		
 		 
-//		 saudacao.setText(saudacao.getText() + "Roberto");
+		
 
 	}
 
