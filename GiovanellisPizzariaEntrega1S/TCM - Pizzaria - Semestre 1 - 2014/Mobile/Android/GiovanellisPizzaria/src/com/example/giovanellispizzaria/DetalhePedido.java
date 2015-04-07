d674df0ffce9d6bde3fe7b053112909e698e0ed6
arthurborbalopes <arthurborbalopes@gmail.com>
2015-04-07 18:28:23 +0000

    Android

--- GiovanellisPizzariaEntrega1S/TCM - Pizzaria - Semestre 1 - 2014/Mobile/Android/GiovanellisPizzaria/src/com/example/giovanellispizzaria/DetalhePedido.java
+++ GiovanellisPizzariaEntrega1S/TCM - Pizzaria - Semestre 1 - 2014/Mobile/Android/GiovanellisPizzaria/src/com/example/giovanellispizzaria/DetalhePedido.java
@@ -1,103 +1,15 @@
 package com.example.giovanellispizzaria;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
-import android.util.Log;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
 
 public class DetalhePedido extends Activity {
 
-	TextView detalhesPedido;
-	TextView tituloPedido;
-	TextView pagamento;
-	TextView troco;
-	
-	Button nomeCliente;
-
-	ListView produtos;
-	
-	Random r = new Random();
-	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) 
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detalhe_pedido);
-
-		try
-		{
-			nomeCliente = (Button) findViewById(R.id.btnCliente);
-			tituloPedido = (TextView) findViewById(R.id.txtTituloPedido);
-			detalhesPedido = (TextView) findViewById(R.id.txtDetalhes);
-			pagamento = (TextView) findViewById(R.id.txtPagamento);
-			troco = (TextView) findViewById(R.id.txtTroco);
-			
-			detalhesPedido.setText(detalhesPedido.getText() + Home.endereco);
-			
-			int formaDePagamento = r.nextInt(3);
-			
-			switch(formaDePagamento)
-			{
-				case 0:
-					troco.setText("Cartão");
-					break;
-					
-				case 1:
-					troco.setText("Cheque");
-					break;
-					
-				case 2:
-					troco.setText("Dinheiro - R$ 60,00 (Troco: R$ 5,27)");
-					break;
-			}
-			
-			
-			
-			String produtosPreco = "";
-			produtosPreco = String.valueOf(Home.qtdProdutos) + " produto";
-			
-			if(Home.qtdProdutos > 1)
-				produtosPreco += "s";
-
-			tituloPedido.setText(produtosPreco);
-		
-			produtos = (ListView) findViewById(R.id.lstProdutos);
-
-			final ArrayList<String> arrayProdutos = new ArrayList<String>();
-			
-			ArrayAdapter<String> adapter2 = new ArrayAdapter<String>
-			(
-				this,
-				android.R.layout.simple_list_item_1,
-				arrayProdutos
-			);
-			
-			
-				produtos.setAdapter(adapter2);
-
-				int j = r.nextInt(6)+1;
-				
-				Log.d("Home", j+"");
-				
-				for(int i = 0; i <  j ; i++)
-					arrayProdutos.add("Pizza " + (i+1));
-
-				adapter2.notifyDataSetChanged();
-		}
-		catch(Exception e)
-		{
-			Log.d("Detalhes Pedido", e.getLocalizedMessage());
-		}
-		
-		
 	}
 
 	@Override
--- GiovanellisPizzariaEntrega1S/TCM - Pizzaria - Semestre 1 - 2014/Mobile/Android/GiovanellisPizzaria/src/com/example/giovanellispizzaria/Home.java
+++ GiovanellisPizzariaEntrega1S/TCM - Pizzaria - Semestre 1 - 2014/Mobile/Android/GiovanellisPizzaria/src/com/example/giovanellispizzaria/Home.java
@@ -1,8 +1,6 @@
 package com.example.giovanellispizzaria;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -10,131 +8,108 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Debug;
-import android.util.Log;
+import android.text.DynamicLayout;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Home extends Activity {
 
-	Context context;
+	Context context = this;
 	
 	TextView historico;
 	TextView pedido;
 	
-	ListView pedidos;
+//	int pedidos = 4;
 	
-	String alertMessage = "";
-	
-	public static String nomePedido;
-	public static String endereco;
-	public static int qtdProdutos;
+	 //ListView list;
+	 //private List<String> List_file;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 		
-		Random r = new Random();
+		pedido = (TextView) findViewById(R.id.txtDetalhes);
+		historico = (TextView) findViewById(R.id.txtHistorico);		
 		
-		context = getApplicationContext();
-		
-		historico = (Button) findViewById(R.id.btnHistorico);
-		
-		pedidos = (ListView) findViewById(R.id.lstPedidos);
-		
-		AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-		
-		builder1.setMessage("Marcar esse pedido como...");
-        builder1.setCancelable(true);
-        builder1.setPositiveButton
-        (
-    		"Entregue", new DialogInterface.OnClickListener() 
-    		{
-    			public void onClick(DialogInterface dialog, int id) 
-    			{
-    				dialog.cancel();
-    			}
-    		}
-		);
-        
-        builder1.setNegativeButton
-        (
-			"Cancelado", new DialogInterface.OnClickListener() 
+		pedido.setOnLongClickListener
+		(
+			new View.OnLongClickListener()
 			{
-				public void onClick(DialogInterface dialog, int id) 
+				public boolean onLongClick(View v)
 				{
-					dialog.cancel();
+					AlertDialog.Builder alertDialog2 = new AlertDialog.Builder(
+					        context);
+
+					// Setting Dialog Title
+					alertDialog2.setTitle("Finalizar pedido");
+
+					alertDialog2.setMessage("Esse pedido foi...");
+
+					alertDialog2.setPositiveButton
+					("Cancelado",
+					        new DialogInterface.OnClickListener() 
+							{
+									public void onClick(DialogInterface dialog, int which) 
+					            	{
+					                // Write your code here to execute after dialog
+					                Toast.makeText(getApplicationContext(),
+					                        "O pedido foi marcado CANCELADO.", Toast.LENGTH_SHORT)
+					                        .show();
+					                dialog.cancel();
+					            	}
+					        }
+						);
+					
+					alertDialog2.setNegativeButton
+					("Entregue",
+					        new DialogInterface.OnClickListener() 
+							{
+					            public void onClick(DialogInterface dialog, int which) 
+					            {
+					                // Write your code here to execute after dialog
+					                Toast.makeText(getApplicationContext(),
+					                		"O pedido foi marcado ENTREGUE.", Toast.LENGTH_SHORT)
+					                        .show();
+					            }
+					        }
+					);
+
+					alertDialog2.show();
+					
+					return true;
 				}
 			}
 		);
-
-        final AlertDialog alert11 = builder1.create();
-
-		final ArrayList<String> arrayPedidos = new ArrayList<String>();
 		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>
-		(
-			this,
-			android.R.layout.simple_list_item_1, 
-			arrayPedidos
-		);
-		
-		pedidos.setAdapter(adapter);
-        
-		for(int i = 0; i < (r.nextInt(10)+1); i++)
-		{
-			endereco = "Endereço " + (i+1);
-			qtdProdutos = (r.nextInt(5)+1);
-			arrayPedidos.add( (i+1) + "- " + endereco + " - " + qtdProdutos  + " produtos");
-		}
-			
-		
-		adapter.notifyDataSetChanged();
-		
-		pedidos.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
-					long arg3) {
-				
-				nomePedido = arrayPedidos.get(position).toString().substring(3);
-				
-				Intent i = new Intent(context, DetalhePedido.class);
-				startActivity(i);
-				
-			}
-		});
-
-		pedidos.setOnItemLongClickListener
-		(
-			new AdapterView.OnItemLongClickListener() 
-			{
-			    @Override
-			    public boolean onItemLongClick(AdapterView<?> av, View v, int pos, long id) 
-			    {
-						alert11.show();	
-			    	return true;
-			    }
-			}
-		);
-
-
-			historico.setOnClickListener
+		pedido.setOnClickListener
 		(
 			new View.OnClickListener()
 			{
 				public void onClick(View v)
 				{
+					/*					
+					Context context = getApplicationContext();
+					Intent i = new Intent(context, DetalhePedido.class);
+					startActivity(i);
+					*/
+				}
+			}
+		);
+		
+		historico.setOnClickListener
+		(
+			new View.OnClickListener()
+			{
+				public void onClick(View v)
+				{
+					Context context = getApplicationContext();
 					Intent i = new Intent(context, Historico.class);
 					startActivity(i);
 				}
