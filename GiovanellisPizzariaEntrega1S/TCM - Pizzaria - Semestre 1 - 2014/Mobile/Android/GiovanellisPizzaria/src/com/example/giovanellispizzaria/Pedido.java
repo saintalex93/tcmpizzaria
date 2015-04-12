4176886efa329d8d58e3f72299b6b4b3309bd4ec
arthurborbalopes <arthurborbalopes@gmail.com>
2015-04-12 15:27:07 +0000

    Atualizações

---
+++ GiovanellisPizzariaEntrega1S/TCM - Pizzaria - Semestre 1 - 2014/Mobile/Android/GiovanellisPizzaria/src/com/example/giovanellispizzaria/Pedido.java
@@ -0,0 +1,18 @@
+package com.example.giovanellispizzaria;
+
+import java.sql.Date;
+import java.util.List;
+
+import android.text.format.Time;
+
+public class Pedido 
+{
+	public int codPedido;
+	public static Date date;
+	public static Time hora;
+	public static double valor;
+	public static int codFuncionario;
+	public static int codCiente;
+	public static String Observacao;
+	
+}
