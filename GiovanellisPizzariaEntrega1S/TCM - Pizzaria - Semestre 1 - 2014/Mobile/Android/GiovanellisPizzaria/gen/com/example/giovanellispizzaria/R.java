593bfdf1def0d41cf8586f8b55ceed06947ffa59
arthurborbalopes <arthurborbalopes@gmail.com>
2015-05-12 18:12:08 +0000

    Android - layout

--- GiovanellisPizzariaEntrega1S/TCM - Pizzaria - Semestre 1 - 2014/Mobile/Android/GiovanellisPizzaria/gen/com/example/giovanellispizzaria/R.java
+++ GiovanellisPizzariaEntrega1S/TCM - Pizzaria - Semestre 1 - 2014/Mobile/Android/GiovanellisPizzaria/gen/com/example/giovanellispizzaria/R.java
@@ -29,29 +29,30 @@ public final class R {
         public static final int marca=0x7f020004;
     }
     public static final class id {
-        public static final int Pedidos=0x7f08000b;
-        public static final int action_settings=0x7f080016;
-        public static final int btnCel=0x7f080004;
-        public static final int btnLogin=0x7f08000f;
-        public static final int btnRes=0x7f080008;
-        public static final int imageView1=0x7f080011;
-        public static final int lstHistorico=0x7f080009;
-        public static final int lstPedidos=0x7f08000a;
-        public static final int lstProdutos=0x7f080003;
-        public static final int rowtext=0x7f080015;
-        public static final int s=0x7f080010;
-        public static final int scrollView1=0x7f080014;
-        public static final int tvTeste=0x7f080012;
-        public static final int txtDetalhes=0x7f080007;
-        public static final int txtHoraPreco=0x7f080002;
-        public static final int txtLogin=0x7f08000e;
-        public static final int txtNome=0x7f080006;
-        public static final int txtPagamento=0x7f080001;
-        public static final int txtSenha=0x7f08000d;
-        public static final int txtTeste=0x7f080013;
-        public static final int txtTituloPedido=0x7f080005;
-        public static final int txtTroco=0x7f080000;
-        public static final int txtUser=0x7f08000c;
+        public static final int Pedidos=0x7f08000c;
+        public static final int action_settings=0x7f080017;
+        public static final int btnCel=0x7f080001;
+        public static final int btnLogin=0x7f080010;
+        public static final int btnRes=0x7f080004;
+        public static final int dados=0x7f080003;
+        public static final int imageView1=0x7f080012;
+        public static final int lstHistorico=0x7f08000a;
+        public static final int lstPedidos=0x7f08000b;
+        public static final int lstProdutos=0x7f080000;
+        public static final int rowtext=0x7f080016;
+        public static final int s=0x7f080011;
+        public static final int scrollView1=0x7f080015;
+        public static final int tvTeste=0x7f080013;
+        public static final int txtDetalhes=0x7f080005;
+        public static final int txtHoraPreco=0x7f080008;
+        public static final int txtLogin=0x7f08000f;
+        public static final int txtNome=0x7f080009;
+        public static final int txtPagamento=0x7f080007;
+        public static final int txtSenha=0x7f08000e;
+        public static final int txtTeste=0x7f080014;
+        public static final int txtTituloPedido=0x7f080002;
+        public static final int txtTroco=0x7f080006;
+        public static final int txtUser=0x7f08000d;
     }
     public static final class layout {
         public static final int activity_detalhe_pedido=0x7f030000;
